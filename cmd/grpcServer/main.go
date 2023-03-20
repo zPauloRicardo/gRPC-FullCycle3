65417e1c855d8cc5800f0ef23c6aeed5032c0360
paulo <pauloricardo.jr1@gmail.com>
2023-03-19 21:09:53 -0300

    feat: created grpc server

---
+++ cmd/grpcServer/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"FcCeS/internal/database"
+	"FcCeS/internal/pb"
+	"FcCeS/internal/service"
+	"database/sql"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"net"
+)
+
+func main() {
+	db, err := sql.Open("sqlite3", "./db.sqlite")
+	if err != nil {
+		panic(err)
+	}
+	defer func(db *sql.DB) {
+		err := db.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(db)
+
+	categoryDb := database.NewCategory(db)
+	categoryService := service.NewCategoryService(*categoryDb)
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterCategoryServiceServer(grpcServer, categoryService)
+	reflection.Register(grpcServer)
+
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		panic(err)
+	}
+
+	if err := grpcServer.Serve(lis); err != nil {
+		panic(err)
+	}
+}
