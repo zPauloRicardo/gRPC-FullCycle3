26c3faa95b4faa9f02919e8a087e4479b3fb464f
Paulo Junior <pauloricardo.jr1@gmail.com>
2023-04-09 18:07:08 -0300

    feat: list categories

--- internal/service/category.go
+++ internal/service/category.go
@@ -15,17 +15,35 @@ func NewCategoryService(categoryDB database.Category) *CategoryService {
 	return &CategoryService{CategoryDB: categoryDB}
 }
 
-func (c *CategoryService) CreateCategory(_ context.Context, in *pb.CreateCategoryRequest) (*pb.CategoryResponse, error) {
+func (c *CategoryService) CreateCategory(_ context.Context, in *pb.CreateCategoryRequest) (*pb.Category, error) {
 	category, err := c.CategoryDB.Create(in.Name, in.Description)
 	if err != nil {
 		return nil, err
 	}
-	categoryResponse := &pb.CategoryResponse{
-		Category: &pb.Category{
+	categoryResponse := &pb.Category{
+		Id:          category.ID,
+		Name:        category.Name,
+		Description: category.Description,
+	}
+	return categoryResponse, nil
+}
+
+func (c *CategoryService) ListCategory(_ context.Context) (*pb.CategoryList, error) {
+	categories, err := c.CategoryDB.FindAll()
+	if err != nil {
+		return nil, err
+	}
+
+	var categoriesResponse []*pb.Category
+
+	for _, category := range categories {
+		categoryResponse := &pb.Category{
 			Id:          category.ID,
 			Name:        category.Name,
 			Description: category.Description,
-		},
+		}
+		categoriesResponse = append(categoriesResponse, categoryResponse)
 	}
-	return categoryResponse, nil
+
+	return &pb.CategoryList{Categories: categoriesResponse}, nil
 }
